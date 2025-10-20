3814cd93f02b7631e2b9a9a33c350e8660c9c871
anastasiia.borisova <anastasiia.borisova@b2broker.com>
2025-10-20 18:21:37 +0200

    add eventing class

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import { User } from './models/User';
 import axios from 'axios';
 
-const user = new User({ id: 12 });
-user.fetch();
-setTimeout(() => {
-  console.log(user);
-}, 4000);
+const user = new User({ name: 'new record', age: 0 });
+
+//user.set({ name: 'NEW', age: 8888 });
+user.save();
 
 //axios.post('http://localhost:3000/users', { name: 'maname', age: 20 });
 //axios.get('http://localhost:3000/users/1');
